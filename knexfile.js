0aba0ab1e0f9066fb909d78cfc39e4167ffa867b
Schrese <53787633+Schrese@users.noreply.github.com>
2020-05-20 19:52:12 -0400

    fixed knexfile user value

--- knexfile.js
+++ knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      user: process.env.DB_PORT,
+      user: process.env.DB_USER,
       database: process.env.DB_DATABASE,
       password: process.env.DB_PASSWORD,
     },
